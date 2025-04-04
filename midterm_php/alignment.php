da9daf0838883da70717666e35ad3bbdeef49c07
deliarobbie <143220462+deliarobbie@users.noreply.github.com>
2025-04-04 15:15:31 -0400

    initial commit, nav bar

---
+++ midterm_php/alignment.php
@@ -0,0 +1,8 @@
+<?php 
+    $title = 'Alignment';
+    $filename = 'alignment.php';
+    include('assets/inc/nav.php');
+?>
+<p>alignment</p>
+    </body>
+</html>
---
+++ midterm_php/proximity.php
@@ -0,0 +1,8 @@
+<?php 
+    $title = 'Proximity';
+    $filename = 'proximity.php';
+    include('assets/inc/nav.php');
+?>
+<p>proximity</p>
+    </body>
+</html>
