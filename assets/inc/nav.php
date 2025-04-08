9801dbc1b4ad2db91ea83540191478b8bf9b7f8a
beegrass <vas8601@rit.edu>
2025-04-08 09:54:12 -0400

    mobile nav trying to do dropdown

--- assets/inc/nav.php
+++ assets/inc/nav.php
@@ -1,28 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="assets/css/styles.css">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bebas+Neue">
-        <title><?php echo $title ?></title>
-    </head>
-    <body>
-        <header>
-                <a href="index.php"><img src="assets/images/shoot_logo.png" alt="Shoot Logo"></a>
-                <nav>
-                    <a href="contrast.php">Contrast</a>
-                    <a href="repetition.php">Repetition</a>
-                    <a href="alignment.php">Alignment</a>
-                    <a href="proximity.php">Proximity</a>
 
-                    <div class="dropdown">
-                        <a class="dropbtn">Resources</a>
-                        <div class="dropdown-content">
-                            <a href="quiz.php">Quiz</a>
-                            <a href="works_cited.php">Works Cited</a>
-                        </div>
-                    </div>
-                </nav>
-            </header>
+<!-- https://stackoverflow.com/questions/59820346/how-to-turn-the-navbar-into-a-hamburger-menu-when-on-mobile -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bebas+Neue">
+    <title><?php echo $title ?></title>
+</head>
+
+<body>
+    <header class="header">
+        <a href="index.php" class="logo"></a>
+        <input class="menu-btn" type="checkbox" id="menu-btn">
+        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+        <ul class="menu">
+            <li><a href="contrast.php">Contrast</a></li>
+            <li><a href="repetition.php">Repetition</a></li>
+            <li><a href="alignment.php">Alignment</a></li>
+            <li><a href="proximity.php">Proximity</a></li>
+            <li class="dropdown">
+                <a class="dropbtn">Resources</a>
+                <ul class="dropdown-content">
+                    <li><a href="quiz.php">Quiz</a></li>
+                    <li><a href="works_cited.php">Works Cited</a></li>
+                </ul>
+            </li>
+        </ul>
+    </header>
