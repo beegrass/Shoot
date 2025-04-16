21a697bfbab05c558da816d3f6cf64490fe33827
beegrass <vas8601@rit.edu>
2025-04-15 20:27:39 -0400

    NAV COMPLETEEEEEE

--- assets/inc/nav.php
+++ assets/inc/nav.php
@@ -13,21 +13,34 @@
 </head>
 
 <body>
-    <header class="header">
-        <a href="index.php" class="logo"></a>
-        <input class="menu-btn" type="checkbox" id="menu-btn">
-        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-        <ul class="menu">
-            <li><a href="contrast.php">Contrast</a></li>
-            <li><a href="repetition.php">Repetition</a></li>
-            <li><a href="alignment.php">Alignment</a></li>
-            <li><a href="proximity.php">Proximity</a></li>
-            <li class="dropdown">
-                <a class="dropbtn">Resources</a>
-                <ul class="dropdown-content">
-                    <li><a href="quiz.php">Quiz</a></li>
-                    <li><a href="works_cited.php">Works Cited</a></li>
-                </ul>
-            </li>
-        </ul>
+    <header>
+        <nav class="navbar">
+            <div class="branding">
+                <a href="index.php" class="logo"></a>
+            </div>
+            <label for="input-hamburger" class="hamburger"><span class="navicon"></span></label>
+            <input type="checkbox" id="input-hamburger" class="hamburger-btn" hidden>
+            <ul class="menu">
+                <li><a href="contrast.php" class="menu-link">Contrast</a></li>
+                <li><a href="alignment.php" class="menu-link">Repetition</a></li>
+                <li><a href="repetition.php" class="menu-link">Alignment</a></li>
+                <li><a href="proximity.php" class="menu-link">Proximity</a></li>
+                <li class="has-dropdown">
+                    <a href="#" class="menu-link">Resources
+                        <span class="arrow"></span>
+                    </a>
+                    <ul class="submenu">
+                        <li><a href="quiz.php" class="menu-link">Quiz</a></li>
+                        <li><a href="works_cited.php" class="menu-link">Work Cited</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
     </header>
+
+    <script>
+        const hamburger = document.querySelector('.hamburger');
+        hamburger.addEventListener('click', function() {
+            this.classList.toggle('close');
+        })
+    </script>
