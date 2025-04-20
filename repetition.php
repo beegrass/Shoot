0ce1400b627bb6c9b43bdee42ac1578956136b1b
beegrass <vas8601@rit.edu>
2025-04-19 23:59:44 -0400

    some more styling

--- repetition.php
+++ repetition.php
@@ -9,15 +9,18 @@ include('assets/inc/nav.php');
 ?>
 
 <!-- Repetition Unique Stuffs -->
-<h2 class="page-section">In Practice</h2>
-<div class="practice">
-    <p>Let's take a look at a website with no repetition...</p>
-    <img src="assets/images/arngren.png" alt="a section of the Arngren website">
-    <p>This website utilizes different font types, sizes, and colors. The images used are also different styles. Though there's a lot of contrast, there's little to no repetition and it's hard to read and navigate the site.</p>
-    <p>Now let's look at a clip of an improved redesign of this website...</p>
-    <img src="assets/images/argrenRedesign.png" alt="a neat redesign of the Arngren website">
-    <p>What about this new design is better? Let's go over it.</p>
-    <p>To start, this design uses a set color palette and typeface to separate different UI elements. Notice how item information is now grouped into cards? Cards are a great way to make different UI elements distinct from one another while ensuring elements with the same functionality all look the same. Now we can easily read what the trending items are.</p>
+<div class="main-content">
+    <h2 class="section-title">In Practice</h2>
+    <div class="practice">
+        <p>Let's take a look at a website with no repetition...</p>
+        <img src="assets/images/arngren.png" alt="a section of the Arngren website">
+        <p>This website utilizes different font types, sizes, and colors. The images used are also different styles. Though there's a lot of contrast, there's little to no repetition and it's hard to read and navigate the site.</p>
+        <p>Now let's look at a clip of an improved redesign of this website...</p>
+        <img src="assets/images/argrenRedesign.png" alt="a neat redesign of the Arngren website">
+        <p>What about this new design is better? Let's go over it.</p>
+        <p>To start, this design uses a set color palette and typeface to separate different UI elements. Notice how item information is now grouped into cards? Cards are a great way to make different UI elements distinct from one another while ensuring elements with the same functionality all look the same. Now we can easily read what the trending items are.</p>
+    </div>
 </div>
+
 
 <?php include('assets/inc/footer.php'); ?>
