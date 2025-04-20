a042ccbdb1c4fa94e2e5b0d2b378d30c40ee5f5f
beegrass <vas8601@rit.edu>
2025-04-20 00:08:58 -0400

    fill in vars for template

--- repetition.php
+++ repetition.php
@@ -2,10 +2,29 @@
 $title = 'Repetition';
 $filename = 'repetition.php';
 include('assets/inc/nav.php');
-?>
 
-<!-- tentative page template include -->
-<?php //include('assets/inc/principle-page.php'); 
+$bannerImg = "url('assets/images/repetitionBanner.png')";
+$bannerTxt = strtoupper($title);
+
+$description = "<b>Repetition</b> refers to the consistent use of design elements in a site. Some elements that should use consistency are the colors, fonts, the general layout and the imagery included.";
+$featureImg = "url('assets/images/repetitionImg.png')";
+
+$descList = array(
+    "Typography Consistency: Using the same fonts and styles for headings, body text, and buttons.",
+    "Color Scheme: Maintaining a consistent palette across the website.",
+    "UI Elements: Keeping button styles, icons, and navigation menus uniform.",
+    "Spacing & Layout: Ensuring consistent padding, margins, and grid structures."
+);
+
+$importance = "Repetition creates cohesion and unity among your pages, which makes the site easier to navigate and aesthetically pleasing. <br><br> 
+                Repetition enhances readability by creating a structured layout, creating familiarity for the user to guide the eye and improve the flow of your content reading. <br><br>
+                A consistent structure can help users predict where key elements are, such as the nav bar, creating a better experience for users.";
+
+$accessibility = "All users can benefit from the proper implementation of repetition. Through the consistency and predictability of a site’s layout, content structure, and navigation, the cognitive load on a user is decreased, as they will only need to take in those elements once and will better retain the information that a site is conveying. <br><br>
+                    A repetition of style can also benefit those with color blindness, as they won’t need to interpret the color schemes of each individual page.";
+
+// tentatively adding in the page template
+//include('assets/inc/principle-page.php'
 ?>
 
 <!-- Repetition Unique Stuffs -->
