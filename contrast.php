a3911fa0ec17aa2a7ef8aa8efd6a18f3e0fc3258
Ryan Morley <160885150+rmore15@users.noreply.github.com>
2025-05-04 22:06:09 -0400

    fixed content for contrast

--- contrast.php
+++ contrast.php
@@ -16,14 +16,29 @@ $descList = array(
     "<b>Spatial Contrast:</b> using white-space to separate and emphasize content"
 );
 
-$importance = "Repetition creates cohesion and unity among your pages, which makes the site easier to navigate and aesthetically pleasing. <br><br> 
-                    Repetition enhances readability by creating a structured layout, creating familiarity for the user to guide the eye and improve the flow of your content reading. <br><br>
-                    A consistent structure can help users predict where key elements are, such as the nav bar, creating a better experience for users.";
+$importance = "Contrast is crucial to make websites accessible to people with visual impairments like color blindness, low 
+            vision, and cognitive disabilities. Besides making websites readable, it 
+            also guides user attention and can be used to create a visual hierarchy. The contrast principle should be properly implemented in all webpages for the benefit of the reader as well as for the one who created the webpage.
+        As previously stated, contrast greatly improves the readability of a website/webpage by allowing readers to easily digest information on a page without having to strain their eyes or focus in on certain areas that have blended in with the background.
+        It also helps readers to discern the overall layout and hierarchy of a website/webpage, allowing the readers to discover and understand important elements and concepts of the website that the author is trying to convey, increasing overall understanding. Lastly, readers who have visual impairments will have difficulty
+        reading through the content of a website when contrast isn't being properly applied. Therefore, by following the accessibility standards below, the author of the website can increase its usability across a wider audience.";
 
 $whyUse = "";
 
-$accessibility = "All users can benefit from the proper implementation of repetition. Through the consistency and predictability of a site's layout, content structure, and navigation, the cognitive load on a user is decreased, as they will only need to take in those elements once and will better retain the information that a site is conveying. <br><br>
-                        A repetition of style can also benefit those with color blindness, as they won't need to interpret the color schemes of each individual page.";
+$accessibility = "<p>The Web Accessibility Guidelines (WCAG) set standards to ensure digital content is accessible to everyone. Some key requirements include:</p><br>
+                <p>Minimum Contrast for Text:</p>
+                <ul>
+                    <li>Regular-sized text (below 18pt or 14pt bold) must have a contrast ratio of at least 4.5:1 against its background</li>
+                    <li>Large-text (18pt or 14pt bold and above) must have a minimum ratio of 3:1</li>
+                </ul><br>
+                <p>Non-text Elements:</p>
+                <ul>
+                    <li>Icons, buttons, and graphical elements must have a contrast ratio of at least 3:1</li>
+                </ul><br>
+                <p>Avoid Color-Only Indicators:</p>
+                <ul>
+                    <li>Color should never be used as the only means of conveying information, indicating an action, prompting a response, or distinguishing a visual element</li>
+                </ul>";
 
 include('assets/inc/principle.php');
 ?>
